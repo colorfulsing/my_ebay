21b842eca0ede1e92aeb24834806075d5a416225
ER <colorfulsing@gmail.com>
2020-11-03 11:22:10 -0600

    fix products and add exporter

--- parsers/product.rb
+++ parsers/product.rb
@@ -2,6 +2,7 @@ html = Nokogiri.HTML(content)
 
 price = html.css('[itemprop="price"]').text
 price = html.css('.vi-VR-cvipPrice').text if price.strip == ''
+price = html.css('.display-price').text if price.strip == ''
 raise 'something happend!' if price.strip == ''
 
 outputs << {
